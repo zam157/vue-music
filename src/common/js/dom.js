88b3ea9b45ce172a2365aecef32f9490bc6aa16b
wt <503369325@qq.com>
2019-11-02 21:57:38 +0800

    add slider & common/js/dom.js

---
+++ src/common/js/dom.js
@@ -0,0 +1,13 @@
+export function addClass (el, className) {
+  if (hasClass(el, className)) {
+    return
+  }
+  let newClass = el.className.split(' ')
+  newClass.push(className)
+  el.className = newClass.join(' ')
+}
+
+export function hasClass (el, className) {
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)') // className前后可以跟空白字符
+  return reg.test(el.className)
+}
