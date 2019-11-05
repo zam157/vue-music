0c4812388b3e2c261492a08012d3c2df07778de3
wt <503369325@qq.com>
2019-11-05 16:34:02 +0800

    Finish recommend page

---
+++ src/base/loading/loading.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="loading">
+    <img class="loading-img" width="24" height="24" src="./loading.png"/>
+    <p class="desc">{{title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '正在载入...'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .loading
+    width 100%
+    text-align center
+    .desc
+      line-height: 20px
+      font-size $font-size-small
+      color: $color-text-l
+    .loading-img
+      display inline
+      animation: 1.5s loading linear infinite;
+    @keyframes loading {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
+    <scroll ref="scroll" class="recommend-content" :data="discList">
       <div>
         <div v-if="recommends.length" class="slider-wrapper">
           <slider>
             <div v-for="item in recommends" :key="item.id">
               <a :href="item.linkUrl">
-                <img :src="item.picUrl"/>
+                <img @load="loadImage" :src="item.picUrl"/>
               </a>
             </div>
           </slider>
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="(item,index) in discList" :key="index" class="item">
               <div class="icon">
-                <img width="60" height="60" :src="item.imgurl"/>
+                <img width="60" height="60" v-lazy="item.imgurl"/>
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
@@ -26,11 +26,16 @@
           </ul>
         </div>
       </div>
-    </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script>
+import Loading from 'base/loading/loading'
+import Scroll from 'base/scroll/scroll'
 import Slider from 'base/slider/slider'
 import {getRecommend, getDiscList} from 'api/recommend'
 import {ERR_OK} from 'api/config'
@@ -38,8 +43,8 @@ import {ERR_OK} from 'api/config'
 export default {
   data () {
     return {
-      recommends: [],
-      discList: []
+      recommends: [], // slider内容
+      discList: [] // 推荐歌单内容
     }
   },
   created () {
@@ -60,10 +65,18 @@ export default {
           this.discList = res.data.list
         }
       })
+    },
+    loadImage () {
+      if (!this.checkLoaded) {
+        this.$refs.scroll.refresh()
+        this.checkLoaded = true
+      }
     }
   },
   components: {
-    Slider
+    Slider,
+    Scroll,
+    Loading
   }
 }
 </script>
