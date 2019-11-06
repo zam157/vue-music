6e2d180af7af36a201a4fe05d349ec081e8279fb
wt <503369325@qq.com>
2019-11-06 18:25:12 +0800

    Add singer page and listview component

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,91 @@
 <template>
-  <div>
-      歌手页面
+  <div class="singer">
+    <listview :data="singers"></listview>
   </div>
 </template>
 
 <script>
-export default {
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
+import Singer from 'common/js/singer'
+import Listview from 'base/listview/listview'
 
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+
+export default {
+  data () {
+    return {
+      singers: []
+    }
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList () {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = this._normalizeSinger(res.data.list)
+        }
+      })
+    },
+    _normalizeSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          // 遍历筛选出前HOT_SINGER_LEN的热门歌手
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        const key = item.Findex // 歌手的姓的拼音字母
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+
+        // 将歌手信息按字母分类
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+      // 将map转换为数组，得到有序列表
+      let hot = []
+      let rest = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          rest.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      rest.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(rest)
+    }
+  },
+  components: {
+    Listview
+  }
 }
 </script>
 
-<style lang="stylus" scoped rel="stylesheet/stylus">
-
+<style lang="stylus" scoped>
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
