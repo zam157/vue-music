81e8c0fe721f2ad3273fc8f76221e1416681b1de
wt <503369325@qq.com>
2019-11-07 20:32:51 +0800

    Add list-shortcut

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -11,3 +11,17 @@ export function hasClass (el, className) {
   let reg = new RegExp('(^|\\s)' + className + '(\\s|$)') // className前后可以跟空白字符
   return reg.test(el.className)
 }
+
+/**
+ * 获取或设置元素中'data-'属性的值
+ * @param {Object} el 元素对象
+ * @param {String} name 属性名
+ * @param {String} val 属性值
+ */
+export function getData (el, name, val) {
+  if (val) {
+    el.dataset[name] = val
+  } else {
+    return el.dataset[name]
+  }
+}
